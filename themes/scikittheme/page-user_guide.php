71b2c2fd358efb8fa678486db344279339d263ee
aleksei korobov <aleksei_korobov@mail.ru>
2021-02-21 17:37:26 +0300

    add page user_guide

---
+++ themes/scikittheme/page-user_guide.php
@@ -0,0 +1,19 @@
+
+
+
+<?php
+get_header();
+
+?>
+<?php
+    header_content()
+?>
+
+
+
+<div class="container sk-landing-container pt-3 body" role="main">
+    
+<p>example</p>
+  
+</div>
+<?php get_footer(); ?>
