4d0ade4ff722be977618a8c168095b32a922ff84
Korobov, Alexey <akorobov@kpmg.ru>
2021-04-25 23:36:32 +0300

    add block scikit-topic

---
+++ plugins/scikit-topic/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'abd531ac73a78f27e7d037fbd794ea5c');
