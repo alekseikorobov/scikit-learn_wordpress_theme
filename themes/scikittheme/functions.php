0b614365e6eba151474222403f9fb34f2979a195
alexey korobov <akorobov@kpmg.ru>
2022-04-03 23:38:48 +0300

    style and js set local. And minor fix text

--- themes/scikittheme/functions.php
+++ themes/scikittheme/functions.php
@@ -158,22 +158,18 @@
 
     function style_theme(){
         wp_enqueue_style('style',get_stylesheet_uri());
-
-        wp_enqueue_style('bootstrap','https://scikit-learn.org/stable/_static/css/vendor/bootstrap.min.css');
-        wp_enqueue_style('gallery','https://scikit-learn.org/stable/_static/gallery.css');
-
-        wp_enqueue_style('gallery','https://scikit-learn.org/stable/_static/gallery.css');
-        wp_enqueue_style('gallery-binder','https://scikit-learn.org/stable/_static/gallery-binder.css');
-        wp_enqueue_style('gallery-dataframe','https://scikit-learn.org/stable/_static/gallery-dataframe.css');
-        wp_enqueue_style('theme','https://scikit-learn.org/stable/_static/css/theme.css');
-        
-      
+        wp_enqueue_style('bootstrap',get_stylesheet_directory_uri().'/css/vendor/bootstrap.min.css');
+        wp_enqueue_style('gallery',get_stylesheet_directory_uri().'/css/sg_gallery.css');
+        wp_enqueue_style('gallery-binder',get_stylesheet_directory_uri().'/css/sg_gallery-binder.css');
+        wp_enqueue_style('gallery-dataframe',get_stylesheet_directory_uri().'/css/sg_gallery-dataframe.css');
+        wp_enqueue_style('sg_gallery-rendered-html',get_stylesheet_directory_uri().'/css/sg_gallery-rendered-html.css');
+        wp_enqueue_style('theme',get_stylesheet_directory_uri().'/css/theme.css');
     }
     function scripts_theme(){
-        wp_enqueue_script('documentation_options','https://scikit-learn.org/stable/_static/documentation_options.js');
+        wp_enqueue_script('documentation_options',get_template_directory_uri().'/js/documentation_options.js');
         wp_enqueue_script('jquery');//,'https://scikit-learn.org/stable/_static/jquery.js');
-        wp_enqueue_script('bootstrap','https://scikit-learn.org/stable/_static/js/vendor/bootstrap.min.js',['jquery']);
-        wp_enqueue_script('versionwarning','https://scikit-learn.org/versionwarning.js');        
+        wp_enqueue_script('bootstrap',get_template_directory_uri().'/js/vendor/bootstrap.min.js',['jquery']);
+        wp_enqueue_script('versionwarning',get_template_directory_uri().'/js/versionwarning.js'); 
     }
 
 
--- themes/scikittheme/views/view1.php
+++ themes/scikittheme/views/view1.php
@@ -8,16 +8,16 @@ function header_content(){
                 <h1 class="sk-landing-header text-white text-monospace">scikit-learn</h1>
                 <h4 class="sk-landing-subheader text-white font-italic mb-3">Машинное обучение в Python</h4>
                 <a class="btn sk-landing-btn mb-1" href="getting_started.html" role="button">Быстрый старт</a>
-                <a class="btn sk-landing-btn mb-1" href="https://scikit-learn.org/stable/auto_examples/release_highlights/plot_release_highlights_0_24_0.html" role="button">Релиз с оффициального сайта</a>
+                <a class="btn sk-landing-btn mb-1" href="https://scikit-learn.org/stable/auto_examples/release_highlights/plot_release_highlights_0_24_0.html" role="button">Релиз с официального сайта</a>
                 <a class="btn sk-landing-btn mb-1" href="https://github.com/scikit-learn/scikit-learn" role="button">GitHub</a>
             </div>
             <div class="col-md-6 d-flex">
                 <ul class="sk-landing-header-body">
                 <!-- <li>Simple and efficient tools for predictive data analysis</li> -->
                 <li>Простой и эффективный инструмент для предиктивного анализа данных</li>
-                <li>Доступный для каждого и переиспользуемые в различных контекстах</li>
+                <li>Доступный для каждого и переиспользуемый в различных контекстах</li>
                 <li>Основанный на NumPy, SciPy, и matplotlib</li>
-                <li>Open source проект, комерческое использование - BSD лицензия</li>
+                <li>Open source проект, коммерческое использование - BSD лицензия</li>
                 </ul>
             </div>
             </div>
